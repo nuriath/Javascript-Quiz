2117a26bcacd71d2d0db5873c7ff1a7eff577ec0
Mwangaza Nuriath <mnuriath28@gmail.com>
2018-12-21 11:47:31 +0200

    add a form tag in html file

--- file.js
+++ file.js
@@ -1,1 +1,6 @@
 var optradio = $("input:radio[name=optradio]:checked").val();
+function mytest()
+{
+    var q1=document.forms["myform"]["answer1"].value;
+    var q2=document.forms["myform"]["answer2"].value;
+}
