5493172ad0b169d945a579f71d89ec3a9d5b66dc
Mwangaza Nuriath <mnuriath28@gmail.com>
2018-12-21 17:31:08 +0200

    change the name of values in html file

--- file.js
+++ file.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
 
     $("blanks form").submit(function(event){
         var q1 = $("input:radio[name=answer1]:checked").val();
@@ -8,6 +8,8 @@ $(document).ready(function(){
         var q5 = $("input:radio[name=answer5]:checked").val();
 
         var marks=0;
+    
+}
 
     if(q1=="language"){
         marks +=2;
@@ -28,7 +30,19 @@ event.preventDefault();
 
 
 })
-});
+})*/
 
-
-
+$(document).ready(function (){
+    $('#blanks form').submit(function(event){
+    var marks=0;
+      var blanks=["answer1", "answer2", "answer3","answer4"]
+        blanks.forEach(function(blank) {
+            var userInput = $("input:radio[name=" + blank + "]:checked" ).val();
+                    if(userInput=="B"){
+                    marks = marks+10;
+                    }
+                })
+          alert(marks)
+    event.preventDefault();
+  });
+  });
