3295984f974b3d474c6c289b080a7a3b28030e21
Mwangaza Nuriath <mnuriath28@gmail.com>
2018-12-21 12:02:15 +0200

    delete same divs and put classes in list

--- file.js
+++ file.js
@@ -3,4 +3,17 @@ function mytest()
 {
     var q1=document.forms["myform"]["answer1"].value;
     var q2=document.forms["myform"]["answer2"].value;
+    var q3=document.forms["myform"]["answer3"].value;
+    var q4=document.forms["myform"]["answer4"].value;
+    var marks=0;
+
+    if(q1=="language"){
+        marks +=2;
+    }
+    if(q2=="Interaction"){
+        marks +=2;
+    }
+
+
+
 }
